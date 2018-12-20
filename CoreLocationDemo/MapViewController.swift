853ceeb8ecd07c860961535f566c192b389616e1
Frakcool <frakcool1993@gmail.com>
2018-12-20 16:33:20 -0500

    Added Map & Annotation after an action is clicked in alert

---
+++ CoreLocationDemo/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MapViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Frakcool on 12/20/18.
+//  Copyright © 2018 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var placemark: CLPlacemark?
+    let regionRadius: CLLocationDistance = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let location = placemark?.location
+        centerMapOnLocation(location: location!)
+        addUserAnnotation(location: location!)
+    }
+    
+    func addUserAnnotation(location: CLLocation) {
+        let userAnnotation = MKPointAnnotation()
+        userAnnotation.coordinate = location.coordinate
+        userAnnotation.title = "You are here!"
+        
+        mapView.addAnnotation(userAnnotation)
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
+    }
+}
--- CoreLocationDemo/ViewController.swift
+++ CoreLocationDemo/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBAction func locationButtonPressed(_ sender: Any) {
         LocationManager.shared.getLastLocation { (optionalPlacemark) in
             if let placemark = optionalPlacemark, let city = placemark.locality {
-                self.showAlert(with: "This is where you are!", and: city)
+                self.showAlert(with: "This is where you are!", and: city, on: placemark)
             }
         }
         // save in variable to allow easy access to display
@@ -32,14 +32,23 @@ class ViewController: UIViewController {
         LocationManager.shared.setUpCLManager()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! MapViewController
+        destination.placemark = sender as? CLPlacemark
+    }
+    
     
     // MARK: - Helper functions
-    private func showAlert(with message: String, and title: String) {
+    private func showAlert(with message: String, and title: String, on placemark: CLPlacemark) {
         // create alert controller
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let mapAction = UIAlertAction(title: "Show on Map", style: .default) { _ in
+            self.performSegue(withIdentifier: "mapSegue", sender: placemark)
+        }
         
         alertController.addAction(okAction)
+        alertController.addAction(mapAction)
         self.present(alertController, animated: true, completion: nil)
     }
     
