cc3fa77445256e7d61b52836b95cf81239762f4a
Jeremiah Hawks <mallardknundrum@gmail.com>
2018-12-20 09:06:59 -0500

    Initial Commit

---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Jeremiah Hawks on 12/20/18.
+//  Copyright © 2018 Jeremiah Hawks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
